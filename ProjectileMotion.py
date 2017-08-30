d356e349b25ad4b52ebbbaa73c51cea08a9306e3
bsmigs <bsmigs@gmail.com>
2017-08-29 22:35:09 -0400

    Add files via upload
    Simplified member variables and vectorized quantities which are naturally vectors

--- ProjectileMotion.py
+++ ProjectileMotion.py
@@ -12,15 +12,7 @@ import matplotlib.animation as animation
 from Tkinter import *
 
 class ProjectileMotion:
-    """Projectile Motion Class
-
-    init_params is [x0, y0, v0, theta], where
-    x0 = initial horizontal position (m)
-    y0 = initial vertical position (m)
-    v0 = initial speed (m/s)
-    theta = initial take off angle (deg)
-
-    """
+    
     def __init__(self):
         self.fields = {'mass':1.0, 'g':9.81, 'v0':10, 'theta':45}
         self.origin = (0, 0)
@@ -29,55 +21,12 @@ class ProjectileMotion:
         self.time_elapsed = 0
         self.state = []
 
-        # set up relevant arrays
-        self.x = [self.origin[0]] # horizontal position (m)
-        self.y = [self.origin[1]] # vertical position (m)
-        self.vx = [] # horizontal speed (m/s)
-        self.vy = [] # vertical speed (m/s)
-        self.t = [0] # time (s)
-        self.Fx = [0] # horizontal force (N)
-        self.Fy = [0] # vertical force (N)
-        self.px = [] # horizontal momentum (kgm/s)
-        self.py = [] # vertical momentum (kgm/s)
-        self.K = [] # kinetic energy (J)
-        self.U = [] # potential energy (J)
-
         
     def getInitialVelocities(self):
         """compute the current x,y velocities of the projectile"""
         v0x = self.fields['v0']*np.cos(self.fields['theta']*np.pi/180.)
         v0y = self.fields['v0']*np.sin(self.fields['theta']*np.pi/180.)
         return (v0x, v0y)
-
-
-    def cacheValues(self):
-        # cache values
-        self.x.append(self.state[0])
-        self.y.append(self.state[2])
-        self.vx.append(self.state[1])
-        self.vy.append(self.state[3])
-        self.t.append(self.time_elapsed)
-
-        ##############################
-        # Compute derived quantities
-        ##############################
-        # force
-        Fx = self.fields['mass'] * (self.vx[-1] - self.vx[-2]) / self.dt
-        Fy = self.fields['mass'] * (self.vy[-1] - self.vy[-2]) / self.dt
-        self.Fx.append(Fx)
-        self.Fy.append(Fy)
-
-        # momentum
-        px = self.fields['mass'] * self.vx[-1]
-        py = self.fields['mass'] * self.vy[-1]
-        self.px.append(px)
-        self.py.append(py)
-
-        # energy
-        K = 0.5 * self.fields['mass'] * (np.dot(self.vx[-1], self.vx[-1]) + np.dot(self.vy[-1], self.vy[-1]))
-        U = self.fields['mass'] * np.abs(self.fields['g']) * self.y[-1]
-        self.K.append(K)
-        self.U.append(U)
     
 
     def step(self):
@@ -94,13 +43,35 @@ class ProjectileMotion:
         self.time_elapsed += self.dt
 
         # cache values
-        self.cacheValues()
+        self.t = np.append(self.t, self.time_elapsed)
+        self.pos = np.append(self.pos, [self.state[0], self.state[2]])
+        self.v = np.append(self.v, [self.state[1], self.state[3]])
+
         
+    def computeDerivedQuantities(self):
+        # reshape so that its in rows of x and y components
+        self.pos = self.pos.reshape(len(self.pos)/2, 2)
+        self.v = self.v.reshape(len(self.v)/2, 2)
+        
+        # compute momentum
+        self.p = self.fields['mass'] * self.v
+
+        # compute kinetic energy
+        self.K = 0.5 * self.fields['mass'] * np.sum(np.square(self.v), axis=1)
+
+        # compute potential energy
+        self.U = self.fields['mass'] * np.abs(self.fields['g']) * self.pos[:,1]
+
+        # compute force
+        self.F = np.diff(self.p, axis=0) / self.dt
+        self.F = np.insert(self.F, 0, self.F[0,:], axis=0)
+
 
     def evolve(self):
         t = self.getTimeVec()
         for tt in t:
             self.step()
+        self.computeDerivedQuantities()
         
 
     def setValues(self):
@@ -121,14 +92,13 @@ class ProjectileMotion:
 
         # define initial state
         vels = self.getInitialVelocities()
-        self.vx.append(vels[0])
-        self.vy.append(vels[1])
-        self.px.append(self.fields['mass']*vels[0])
-        self.py.append(self.fields['mass']*vels[1])
-        self.K.append(0.5*self.fields['mass']*(vels[0]*vels[0] + vels[1]*vels[1]))
-        self.U.append(self.fields['mass']*np.abs(self.fields['g'])*self.y[0])
         self.state = [self.origin[0], vels[0], self.origin[1], vels[1]]
-    
+
+        # set initial conditions
+        self.t = np.array([0])
+        self.pos = np.array([self.origin[0], self.origin[1]])
+        self.v = np.array([vels[0], vels[1]])
+        
 
     def maxHeight(self):
         """Get max height of projectile"""
@@ -215,8 +185,8 @@ def runAnimation():
         plt.title("Vertical Position vs. Horizontal Position")
 
         def y_vs_x(i):
-            x = pm.x[0:i+1]
-            y = pm.y[0:i+1]
+            x = pm.pos[0:i+1, 0]
+            y = pm.pos[0:i+1, 1]
             line.set_data(x, y)
             time_text.set_text('time = %.3f (s)' % pm.t[i])
             return line, time_text
@@ -239,7 +209,7 @@ def runAnimation():
 
         def x_vs_t(i):
             t = pm.t[0:i+1]
-            x = pm.x[0:i+1]
+            x = pm.pos[0:i+1, 0]
             line2.set_data(t, x)
             time_text2.set_text('time = %.3f (s)' % pm.t[i])
             return line2, time_text2
@@ -262,7 +232,7 @@ def runAnimation():
 
         def y_vs_t(i):
             t = pm.t[0:i+1]
-            y = pm.y[0:i+1]
+            y = pm.pos[0:i+1, 1]
             line3.set_data(t, y)
             time_text3.set_text('time = %.3f (s)' % pm.t[i])
             return line3, time_text3
@@ -275,7 +245,7 @@ def runAnimation():
     if (CheckVar4.get() == 1):
         fig4 = plt.figure()
         ax4 = fig4.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, pm.totalTime()+0.5), ylim=(min(pm.Fy)-2, max(pm.Fy)+2))
+                            xlim=(0, pm.totalTime()+0.5), ylim=(min(pm.F[:,1])-2, max(pm.F[:,0])+2))
         ax4.grid()
         time_text4 = ax4.text(0.02, 0.95, '', transform=ax4.transAxes)
         line4a, = ax4.plot([], [], '-')
@@ -287,8 +257,8 @@ def runAnimation():
 
         def F_vs_t(i):
             t = pm.t[0:i+1]
-            Fx = pm.Fx[0:i+1]
-            Fy = pm.Fy[0:i+1]
+            Fx = pm.F[0:i+1, 0]
+            Fy = pm.F[0:i+1, 1]
             line4a.set_data(t, Fx)
             line4b.set_data(t, Fy)
             time_text4.set_text('time = %.3f (s)' % pm.t[i])
@@ -299,11 +269,11 @@ def runAnimation():
 
 
 
-    ## set up figure force vs. time animation
+    ## set up figure momentum vs. time animation
     if (CheckVar5.get() == 1):
         fig5 = plt.figure()
         ax5 = fig5.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, pm.totalTime()+0.5), ylim=(min(pm.py)-2, max(pm.py)+2))
+                            xlim=(0, pm.totalTime()+0.5), ylim=(min(pm.p[:,1])-2, max(pm.p[:,1])+2))
         ax5.grid()
         time_text5 = ax5.text(0.02, 0.95, '', transform=ax5.transAxes)
         line5a, = ax5.plot([], [], '-')
@@ -315,8 +285,8 @@ def runAnimation():
 
         def p_vs_t(i):
             t = pm.t[0:i+1]
-            px = pm.px[0:i+1]
-            py = pm.py[0:i+1]
+            px = pm.p[0:i+1, 0]
+            py = pm.p[0:i+1, 1]
             line5a.set_data(t, px)
             line5b.set_data(t, py)
             time_text5.set_text('time = %.3f (s)' % pm.t[i])
@@ -328,7 +298,8 @@ def runAnimation():
 
     ## set up figure energy vs. time animation
     if (CheckVar6.get() == 1):
-        E = [pm.K[i]+pm.U[i] for i in xrange(len(pm.K))]
+        #E = [pm.K[i]+pm.U[i] for i in xrange(len(pm.K))]
+        E = pm.K + pm.U
         
         fig6 = plt.figure()
         ax6 = fig6.add_subplot(111, autoscale_on=False, \
