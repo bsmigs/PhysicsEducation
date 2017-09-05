0a49b86a3f689264434669d32edbce7c7d9b1086
bsmigs <bsmigs@gmail.com>
2017-09-05 06:26:26 -0400

    Add files via upload
    Broke out projectile motion routines from main program execution. Additional cleanups.

---
+++ PMExec.py
@@ -0,0 +1,256 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import Tkinter as tk
+import ProjectileMotion as pm
+from WindowConstruction import *
+
+root = tk.Tk()
+root.title('Particle 1 Parameter Window')
+
+# Ask eventually if we are looking at 1 or 2 particles
+# If 2, we will then compare their performances
+# to one another
+
+# create object
+pm = pm.ProjectileMotion()
+
+# Add basic fields
+entries = loadFields(root, pm.fields, pm.unitFields)
+    
+# Begin logic to include air resistance
+def revealOptions(dragEntries):
+    for ent in dragEntries:
+        if (CheckVar0.get() == 0):
+            ent[1].config(state='disabled')
+        else:
+            ent[1].config(state='normal')
+            
+CheckVar0 = tk.IntVar()
+C0 = tk.Checkbutton(root, text="Include air resistance", justify=tk.LEFT, variable=CheckVar0, command=lambda: revealOptions(dragEntries))
+C0.pack(side=tk.TOP, anchor=tk.W)
+
+dragEntries = loadFields(root, pm.dragFields, pm.unitDragFields, 'disabled')
+# End logic to include air resistance
+
+    
+
+typesOfPlots = ['Y-position vs. X-position', 'X-position vs. Time', 'Y-position vs. Time', \
+                    'Force vs. Time', 'Momentum vs. Time', 'Energy vs. Time']
+
+checkVarList = []
+for plotType in typesOfPlots:
+    CheckVar = tk.IntVar()
+    checkVarList.append(CheckVar)
+    
+    C = tk.Checkbutton(root, text=plotType, variable=CheckVar, justify=tk.LEFT)
+    C.pack(side=tk.TOP, anchor=tk.W)
+    
+
+
+    
+def create_window():
+    window = tk.Toplevel(root)
+
+b = tk.Button(root, text="Create new window", command=create_window)
+b.pack()
+
+
+
+
+def runAnimation(checkVarList):
+
+    # append drag force fields to normal ones
+    # no matter what
+    allEntries = entries + dragEntries
+    
+    # read in and set user values
+    pm.setValues(allEntries)
+
+
+    # ask if we are using drag force
+    usingDragForce = CheckVar0.get()
+    maxTime, maxRange, maxHeight = pm.evolve(usingDragForce)
+    timeIdxs = np.arange(pm.t.size)
+
+    intervalTime = 3000*pm.dt
+
+    
+    ## set up figure y-position vs. x-position animation
+    if (checkVarList[0].get() == 1):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxRange+2), ylim=(0, maxHeight+2))
+        ax.grid()
+        line, = ax.plot([], [], '-')
+        time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+        plt.xlabel("Horizontal position (m)")
+        plt.ylabel("Vertical position (m)")
+        plt.title("Vertical Position vs. Horizontal Position")
+
+        def y_vs_x(i):
+            x = pm.pos[0:i+1, 0]
+            y = pm.pos[0:i+1, 1]
+            line.set_data(x, y)
+            time_text.set_text('time = %.3f (s)' % pm.t[i])
+            return line, time_text
+    
+        ani = animation.FuncAnimation(fig, y_vs_x, frames=timeIdxs, \
+                                    interval=intervalTime, blit=True, repeat=False)
+
+
+    ## set up figure x-position vs. time animation
+    if (checkVarList[1].get()== 1):
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxTime+0.5), ylim=(0, maxRange+2))
+        ax2.grid()
+        time_text2 = ax2.text(0.02, 0.95, '', transform=ax2.transAxes)
+        line2, = ax2.plot([], [], '-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("Horizontal position (m)")
+        plt.title("Horizontal Position vs. Time")
+
+        def x_vs_t(i):
+            t = pm.t[0:i+1]
+            x = pm.pos[0:i+1, 0]
+            line2.set_data(t, x)
+            time_text2.set_text('time = %.3f (s)' % pm.t[i])
+            return line2, time_text2
+
+        ani2 = animation.FuncAnimation(fig2, x_vs_t, frames=timeIdxs, \
+                                    interval=intervalTime, blit=True, repeat=False)
+
+
+    ## set up figure y-position vs. time animation
+    if (checkVarList[2].get() == 1):
+        fig3 = plt.figure()
+        ax3 = fig3.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxTime+0.5), ylim=(0, maxHeight+2))
+        ax3.grid()
+        time_text3 = ax3.text(0.02, 0.95, '', transform=ax3.transAxes)
+        line3, = ax3.plot([], [], '-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("Vertical position (m)")
+        plt.title("Vertical Position vs. Time")
+
+        def y_vs_t(i):
+            t = pm.t[0:i+1]
+            y = pm.pos[0:i+1, 1]
+            line3.set_data(t, y)
+            time_text3.set_text('time = %.3f (s)' % pm.t[i])
+            return line3, time_text3
+
+        ani3 = animation.FuncAnimation(fig3, y_vs_t, frames=timeIdxs, \
+                                        interval=intervalTime, blit=True, repeat=False)
+
+
+    ## set up figure force vs. time animation
+    if (checkVarList[3].get() == 1):
+        fig4 = plt.figure()
+        ax4 = fig4.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxTime+0.5), ylim=(min(pm.F[:,1])-2, max(pm.F[:,0])+2))
+        ax4.grid()
+        time_text4 = ax4.text(0.02, 0.95, '', transform=ax4.transAxes)
+        line4a, = ax4.plot([], [], '-')
+        line4b, = ax4.plot([], [], '-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("Force (N)")
+        plt.title("Force vs. Time")
+        plt.legend(['Fx (horizontal)', 'Fy (vertical)'])
+
+        def F_vs_t(i):
+            t = pm.t[0:i+1]
+            Fx = pm.F[0:i+1, 0]
+            Fy = pm.F[0:i+1, 1]
+            line4a.set_data(t, Fx)
+            line4b.set_data(t, Fy)
+            time_text4.set_text('time = %.3f (s)' % pm.t[i])
+            return line4a, line4b, time_text4
+
+        ani4 = animation.FuncAnimation(fig4, F_vs_t, frames=timeIdxs, \
+                                        interval=intervalTime, blit=True, repeat=False)
+
+
+
+    ## set up figure momentum vs. time animation
+    if (checkVarList[4].get() == 1):
+        fig5 = plt.figure()
+        ax5 = fig5.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxTime+0.5), ylim=(min(pm.p[:,1])-2, max(pm.p[:,1])+2))
+        ax5.grid()
+        time_text5 = ax5.text(0.02, 0.95, '', transform=ax5.transAxes)
+        line5a, = ax5.plot([], [], '-')
+        line5b, = ax5.plot([], [], '-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("Momentum (kgm/s)")
+        plt.title("Momentum vs. Time")
+        plt.legend(['px (horizontal)', 'py (vertical)'])
+
+        def p_vs_t(i):
+            t = pm.t[0:i+1]
+            px = pm.p[0:i+1, 0]
+            py = pm.p[0:i+1, 1]
+            line5a.set_data(t, px)
+            line5b.set_data(t, py)
+            time_text5.set_text('time = %.3f (s)' % pm.t[i])
+            return line5a, line5b, time_text5
+
+        ani5 = animation.FuncAnimation(fig5, p_vs_t, frames=timeIdxs, \
+                                        interval=intervalTime, blit=True, repeat=False)
+
+
+    ## set up figure energy vs. time animation
+    if (checkVarList[5].get() == 1):
+        #E = [pm.K[i]+pm.U[i] for i in xrange(len(pm.K))]
+        E = pm.K + pm.U
+        
+        fig6 = plt.figure()
+        ax6 = fig6.add_subplot(111, autoscale_on=False, \
+                            xlim=(0, maxTime+0.5), ylim=(min(pm.U)-2, max(E)+2))
+        ax6.grid()
+        time_text6 = ax6.text(0.02, 0.95, '', transform=ax6.transAxes)
+        line6a, = ax6.plot([], [], '-')
+        line6b, = ax6.plot([], [], '-')
+        line6c, = ax6.plot([], [], '-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("Energy (J)")
+        plt.title("Energy vs. Time")
+        plt.legend(['Kinetic', 'Potential', 'Total'])
+
+        def E_vs_t(i):
+            t = pm.t[0:i+1]
+            K = pm.K[0:i+1]
+            U = pm.U[0:i+1]
+            Eshort = E[0:i+1]
+
+            line6a.set_data(t, K)
+            line6b.set_data(t, U)
+            line6c.set_data(t, Eshort)
+            time_text6.set_text('time = %.3f (s)' % pm.t[i])
+            return line6a, line6b, line6c, time_text6
+
+        ani6 = animation.FuncAnimation(fig6, E_vs_t, frames=timeIdxs, \
+                                        interval=intervalTime, blit=True, repeat=False)
+
+
+
+    print("Maximum Time = %.3f (s)" % maxTime)
+    print("Maximum Range = %.3f (m)" % maxRange)
+    print("Maximum Height = %.3f (m)" % maxHeight)
+        
+    # show all relevant plots and then quit when finished
+    plt.show()
+    pm.clear()
+
+
+# set up buttons to run
+b1 = tk.Button(root, text='Run', command=lambda l=checkVarList: runAnimation(l))
+b1.pack(side=tk.LEFT, padx=5, pady=5)
+
+# set up buttons to quit
+b2 = tk.Button(root, text='Quit', command=root.quit)
+b2.pack(side=tk.RIGHT, padx=5, pady=5)
+
+# run it
+root.mainloop()
--- ProjectileMotion.py
+++ ProjectileMotion.py
@@ -7,11 +7,9 @@ website: http://rfground.wordpress.com
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import scipy.integrate as integrate
 from scipy import interpolate
-from Tkinter import *
+import Tkinter as tk
 
 class ProjectileMotion:
     
@@ -89,14 +87,19 @@ class ProjectileMotion:
     def derivs(self, dummyState, dummyT):
         area = 0.25 * np.pi * self.fields['diameter']
         dragCoeff = 0.5 * self.fields['drag coefficient'] * self.fields['air density'] * area
-
+        
         # model the quadratic drag force \alpha |v|^2 \hat{v} as \alpha |v| \vec{v}
         # set \beta = \alpha |v|
+        beta = dragCoeff * np.sqrt(dummyState[1]*dummyState[1] + dummyState[3]*dummyState[3])
+
+        # x velocity
         dxdt = dummyState[1]
+        # y velocity
         dydt = dummyState[3]
         
-        beta = dragCoeff * np.sqrt(dummyState[1]*dummyState[1] + dummyState[3]*dummyState[3])
+        # x-acceleration
         dvxdt = -beta * dummyState[1]
+        # y-acceleration
         dvydt = self.fields['g'] - beta * dummyState[3]
 
         return np.array([dxdt, dvxdt, dydt, dvydt])
@@ -119,25 +122,6 @@ class ProjectileMotion:
 
     def evolve(self, usingDragForce):
         t = self.getTimeVec()
-
-        """
-        if (usingDragForce == 0):
-            for tt in t:
-                self.statesNoDrag()
-                
-            # reshape so that its in rows of x and y components
-            self.pos = self.pos.reshape(len(self.pos)/2, 2)
-            self.v = self.v.reshape(len(self.v)/2, 2)
-        else:
-            # integrate to get solutions
-            states = integrate.odeint(self.derivs, self.state, t)
-            states = np.array(states)
-            
-            # break out positions/vels from the state vector
-            self.pos = states[:,[0,2]]
-            self.v = states[:,[1,3]]
-            self.t = t
-        """
 
         if (usingDragForce == 0):
             self.fields['diameter'] = 0
@@ -202,7 +186,6 @@ class ProjectileMotion:
         totalTime *= 10
         totalTime = np.ceil(totalTime)
         totalTime /= 10
-        
         timeVec = np.arange(0,np.ceil(totalTime),self.dt)
         return timeVec
       
@@ -215,285 +198,4 @@ class ProjectileMotion:
     def clear(self):
         self.__init__()
 
-##########################################################
-""" RUN SIM """
-##########################################################
-
-pm = ProjectileMotion()
-root = Tk()
-
-row = Frame(root)
-label = Label(row, width=15, text='POOP', anchor='w')
-label.pack(side=TOP)
-
-entries = []
-for key in pm.fields:
-    row = Frame(root)
-    label = Label(row, width=15, text=key, anchor='w')
-    entry = Entry(row)
-    entry.insert(END, pm.fields[key])
-
-    row.bind()
-    row.pack(side=TOP, fill=X, padx=5, pady=5)
-    label.pack(side=LEFT)
-    entry.pack(side=LEFT, expand=YES, fill=X)
-
-    # set units
-    unitsLabel = Label(row, width=8, text=pm.unitFields[key], anchor='w')
-    unitsLabel.pack(side=RIGHT)
-
-    # append values to entries list
-    entries.append( (key, entry) )
-    
-    
-# Begin logic to include air resistance
-def revealOptions(dragEntries):
-    for ent in dragEntries:
-        if (CheckVar0.get() == 0):
-            ent[1].config(state='disabled')
-        else:
-            ent[1].config(state='normal')
-            
-CheckVar0 = IntVar()
-C0 = Checkbutton(root, text="Include air resistance", justify=LEFT, variable=CheckVar0, command=lambda: revealOptions(dragEntries))
-C0.pack(side=TOP, anchor=W)
-
-dragEntries = []
-for key in pm.dragFields:
-    row = Frame(root)
-    label = Label(row, width=15, text=key, anchor='w')
-    ent = Entry(row)
-    ent.insert(END, pm.dragFields[key])
-
-    row.bind()
-    row.pack(side=TOP, fill=X, padx=5, pady=5)
-    label.pack(side=LEFT)
-    ent.pack(side=LEFT, expand=YES, fill=X)
-    ent.config(state='disabled')
-
-    # set units
-    unitsLabel = Label(row, width=8, text=pm.unitDragFields[key], anchor='w')
-    unitsLabel.pack(side=RIGHT)
-
-    dragEntries.append((key, ent))
-# End logic to include air resistance
-
-
-CheckVar1 = IntVar()
-CheckVar2 = IntVar()
-CheckVar3 = IntVar()
-CheckVar4 = IntVar()
-CheckVar5 = IntVar()
-CheckVar6 = IntVar()
-
-C1 = Checkbutton(root, text="Y-position vs. X-position", variable=CheckVar1, justify=LEFT)
-C2 = Checkbutton(root, text="X-position vs. Time", variable=CheckVar2, justify=LEFT)
-C3 = Checkbutton(root, text="Y-position vs. Time", variable=CheckVar3, justify=LEFT)
-C4 = Checkbutton(root, text="Force vs. Time", variable=CheckVar4, justify=LEFT)
-C5 = Checkbutton(root, text="Momentum vs. Time", variable=CheckVar5, justify=LEFT)
-C6 = Checkbutton(root, text="Energy vs. Time", variable=CheckVar6, justify=LEFT)
-
-C1.pack(side=TOP, anchor=W)
-C2.pack(side=TOP, anchor=W)
-C3.pack(side=TOP, anchor=W)
-C4.pack(side=TOP, anchor=W)
-C5.pack(side=TOP, anchor=W)
-C6.pack(side=TOP, anchor=W)
-
-
-def runAnimation():
-
-    # append drag force fields to normal ones
-    # no matter what
-    allEntries = entries + dragEntries
         
-    # read in and set user values
-    pm.setValues(allEntries)
-
-    # ask if we are using drag force
-    usingDragForce = CheckVar0.get()
-    maxTime, maxRange, maxHeight = pm.evolve(usingDragForce)
-    
-    t_size = pm.t.size
-    timeIdxs = np.arange(t_size)
-
-    if (CheckVar1.get() == 1):
-        ## set up figure y-position vs. x-position animation
-        fig = plt.figure()
-        ax = fig.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxRange+2), ylim=(0, maxHeight+2))
-        ax.grid()
-        line, = ax.plot(pm.origin[0], pm.origin[1], '-')
-        time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
-        plt.xlabel("Horizontal position (m)")
-        plt.ylabel("Vertical position (m)")
-        plt.title("Vertical Position vs. Horizontal Position")
-
-        def y_vs_x(i):
-            x = pm.pos[0:i+1, 0]
-            y = pm.pos[0:i+1, 1]
-            line.set_data(x, y)
-            time_text.set_text('time = %.3f (s)' % pm.t[i])
-            return line, time_text
-    
-        ani = animation.FuncAnimation(fig, y_vs_x, frames=timeIdxs,
-                                    interval=3000*pm.dt, blit=True, repeat=False)
-
-
-    ## set up figure x-position vs. time animation
-    if (CheckVar2.get() == 1):
-        fig2 = plt.figure()
-        ax2 = fig2.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxTime+0.5), ylim=(0, maxRange+2))
-        ax2.grid()
-        time_text2 = ax2.text(0.02, 0.95, '', transform=ax2.transAxes)
-        line2, = ax2.plot([], [], '-')
-        plt.xlabel("Time (s)")
-        plt.ylabel("Horizontal position (m)")
-        plt.title("Horizontal Position vs. Time")
-
-        def x_vs_t(i):
-            t = pm.t[0:i+1]
-            x = pm.pos[0:i+1, 0]
-            line2.set_data(t, x)
-            time_text2.set_text('time = %.3f (s)' % pm.t[i])
-            return line2, time_text2
-
-        ani2 = animation.FuncAnimation(fig2, x_vs_t, frames=timeIdxs,
-                                    interval=3000*pm.dt, blit=True, repeat=False)
-
-
-    ## set up figure y-position vs. time animation
-    if (CheckVar3.get() == 1):
-        fig3 = plt.figure()
-        ax3 = fig3.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxTime+0.5), ylim=(0, maxHeight+2))
-        ax3.grid()
-        time_text3 = ax3.text(0.02, 0.95, '', transform=ax3.transAxes)
-        line3, = ax3.plot([], [], '-')
-        plt.xlabel("Time (s)")
-        plt.ylabel("Vertical position (m)")
-        plt.title("Vertical Position vs. Time")
-
-        def y_vs_t(i):
-            t = pm.t[0:i+1]
-            y = pm.pos[0:i+1, 1]
-            line3.set_data(t, y)
-            time_text3.set_text('time = %.3f (s)' % pm.t[i])
-            return line3, time_text3
-
-        ani3 = animation.FuncAnimation(fig3, y_vs_t, frames=timeIdxs,
-                                        interval=3000*pm.dt, blit=True, repeat=False)
-
-
-    ## set up figure force vs. time animation
-    if (CheckVar4.get() == 1):
-        fig4 = plt.figure()
-        ax4 = fig4.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxTime+0.5), ylim=(min(pm.F[:,1])-2, max(pm.F[:,0])+2))
-        ax4.grid()
-        time_text4 = ax4.text(0.02, 0.95, '', transform=ax4.transAxes)
-        line4a, = ax4.plot([], [], '-')
-        line4b, = ax4.plot([], [], '-')
-        plt.xlabel("Time (s)")
-        plt.ylabel("Force (N)")
-        plt.title("Force vs. Time")
-        plt.legend(['Fx (horizontal)', 'Fy (vertical)'])
-
-        def F_vs_t(i):
-            t = pm.t[0:i+1]
-            Fx = pm.F[0:i+1, 0]
-            Fy = pm.F[0:i+1, 1]
-            line4a.set_data(t, Fx)
-            line4b.set_data(t, Fy)
-            time_text4.set_text('time = %.3f (s)' % pm.t[i])
-            return line4a, line4b, time_text4
-
-        ani4 = animation.FuncAnimation(fig4, F_vs_t, frames=timeIdxs,
-                                        interval=3000*pm.dt, blit=True, repeat=False)
-
-
-
-    ## set up figure momentum vs. time animation
-    if (CheckVar5.get() == 1):
-        fig5 = plt.figure()
-        ax5 = fig5.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxTime+0.5), ylim=(min(pm.p[:,1])-2, max(pm.p[:,1])+2))
-        ax5.grid()
-        time_text5 = ax5.text(0.02, 0.95, '', transform=ax5.transAxes)
-        line5a, = ax5.plot([], [], '-')
-        line5b, = ax5.plot([], [], '-')
-        plt.xlabel("Time (s)")
-        plt.ylabel("Momentum (kgm/s)")
-        plt.title("Momentum vs. Time")
-        plt.legend(['px (horizontal)', 'py (vertical)'])
-
-        def p_vs_t(i):
-            t = pm.t[0:i+1]
-            px = pm.p[0:i+1, 0]
-            py = pm.p[0:i+1, 1]
-            line5a.set_data(t, px)
-            line5b.set_data(t, py)
-            time_text5.set_text('time = %.3f (s)' % pm.t[i])
-            return line5a, line5b, time_text5
-
-        ani5 = animation.FuncAnimation(fig5, p_vs_t, frames=timeIdxs,
-                                        interval=3000*pm.dt, blit=True, repeat=False)
-
-
-    ## set up figure energy vs. time animation
-    if (CheckVar6.get() == 1):
-        #E = [pm.K[i]+pm.U[i] for i in xrange(len(pm.K))]
-        E = pm.K + pm.U
-        
-        fig6 = plt.figure()
-        ax6 = fig6.add_subplot(111, autoscale_on=False, \
-                            xlim=(0, maxTime+0.5), ylim=(min(pm.U)-2, max(E)+2))
-        ax6.grid()
-        time_text6 = ax6.text(0.02, 0.95, '', transform=ax6.transAxes)
-        line6a, = ax6.plot([], [], '-')
-        line6b, = ax6.plot([], [], '-')
-        line6c, = ax6.plot([], [], '-')
-        plt.xlabel("Time (s)")
-        plt.ylabel("Energy (J)")
-        plt.title("Energy vs. Time")
-        plt.legend(['Kinetic', 'Potential', 'Total'])
-
-        def E_vs_t(i):
-            t = pm.t[0:i+1]
-            K = pm.K[0:i+1]
-            U = pm.U[0:i+1]
-            Eshort = E[0:i+1]
-
-            line6a.set_data(t, K)
-            line6b.set_data(t, U)
-            line6c.set_data(t, Eshort)
-            time_text6.set_text('time = %.3f (s)' % pm.t[i])
-            return line6a, line6b, line6c, time_text6
-
-        ani6 = animation.FuncAnimation(fig6, E_vs_t, frames=timeIdxs,
-                                        interval=3000*pm.dt, blit=True, repeat=False)
-
-
-
-    print("Maximum Time = %.3f (s)" % maxTime)
-    print("Maximum Range = %.3f (m)" % maxRange)
-    print("Maximum Height = %.3f (m)" % maxHeight)
-        
-    # show all relevant plots and then quit when finished
-    # TODO: Clear out pm object so sim can run again
-    plt.show()
-    #root.quit()
-    pm.clear()
-
-
-# set up buttons to run
-b1 = Button(root, text='Run', command=runAnimation)
-b1.pack(side=LEFT, padx=5, pady=5)
-
-# set up buttons to quit
-b2 = Button(root, text='Quit', command=root.quit)
-b2.pack(side=RIGHT, padx=5, pady=5)
-
-# run it
-root.mainloop()
